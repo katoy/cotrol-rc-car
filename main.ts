ac12e63116e5c63de800862366e22e2cdad18f80
katoy <youichikato@gmail.com>
2020-11-01 00:56:37 +0900

    fix LED

--- main.ts
+++ main.ts
@@ -3,14 +3,21 @@
  * 
  * https://www.switch-science.com/catalog/5308/
  */
-let R = 0
-let L = 0
+let PREV_R = 0
+let PREV_L = 0
 let DISP_R = 0
 let DISP_L = 0
+let R = 0
+let L = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 radio.setGroup(12)
 basic.forever(function () {
-    led.unplot(0, DISP_L)
-    led.unplot(4, DISP_R)
     // P2:0 左ジョイスティック選択
     // P2:1 右ジョイスティック選択
     pins.digitalWritePin(DigitalPin.P2, 0)
@@ -19,8 +26,18 @@ basic.forever(function () {
     L = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, -100, 100)
     pins.digitalWritePin(DigitalPin.P2, 1)
     R = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, -100, 100)
-    DISP_R = Math.round(Math.map(R, -100, 100, 4, 0))
+    radio.sendValue("L", L)
+    radio.sendValue("R", R)
     DISP_L = Math.round(Math.map(L, -100, 100, 4, 0))
-    led.plot(0, DISP_L)
-    led.plot(4, DISP_R)
+    DISP_R = Math.round(Math.map(R, -100, 100, 4, 0))
+    if (DISP_L != PREV_L) {
+        led.unplot(0, PREV_L)
+        led.plot(0, DISP_L)
+    }
+    if (DISP_R != PREV_R) {
+        led.unplot(4, PREV_R)
+        led.plot(4, DISP_R)
+    }
+    PREV_L = DISP_L
+    PREV_R = DISP_R
 })
