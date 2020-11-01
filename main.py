ad9b09da01d2e50ef14ced31fa4d0ed1b3678535
katoy <youichikato@gmail.com>
2020-11-01 09:15:58 +0900

    using array and function

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+def readVals():
+    for index in range(2):
+        # P2:0 左ジョイスティック選択
+        # P2:1 右ジョイスティック選択
+        pins.digital_write_pin(DigitalPin.P2, index)
+        vals[index] = Math.map(pins.analog_read_pin(AnalogPin.P1), 0, 1023, -255, 255)
+def saveVals():
+    for index2 in range(2):
+        prevVals[index2] = vals[index2]
+def sendVals():
+    for index3 in range(2):
+        radio.send_value(keys[index3], Math.round(vals[index3]))
+def showVals():
+    global col
+    for index4 in range(2):
+        if prevVals[index4] != vals[index4]:
+            col = cols[index4]
+            led.unplot(col, Math.round(Math.map(prevVals[index4], -255, 255, 4, 0)))
+            led.plot(col, Math.round(Math.map(vals[index4], -255, 255, 4, 0)))
 """
 
 ジョイスティック：
@@ -5,26 +24,27 @@
 https://www.switch-science.com/catalog/5308/
 
 """
-R = 0
-L = 0
-DISP_R = 0
-DISP_L = 0
+col = 0
+keys: List[str] = []
+cols: List[number] = []
+prevVals: List[number] = []
+vals: List[number] = []
+basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    """)
 radio.set_group(12)
+vals = [0, 0]
+prevVals = [0, 0]
+cols = [0, 4]
+keys = ["L", "R"]
 
 def on_forever():
-    global L, R, DISP_R, DISP_L
-    led.unplot(0, DISP_L)
-    led.unplot(4, DISP_R)
-    # P2:0 左ジョイスティック選択
-    # P2:1 右ジョイスティック選択
-    pins.digital_write_pin(DigitalPin.P2, 0)
-    # P0は横方向
-    # P1は縦方向
-    L = Math.map(pins.analog_read_pin(AnalogPin.P1), 0, 1023, -100, 100)
-    pins.digital_write_pin(DigitalPin.P2, 1)
-    R = Math.map(pins.analog_read_pin(AnalogPin.P1), 0, 1023, -100, 100)
-    DISP_R = Math.round(Math.map(R, -100, 100, 4, 0))
-    DISP_L = Math.round(Math.map(L, -100, 100, 4, 0))
-    led.plot(0, DISP_L)
-    led.plot(4, DISP_R)
+    saveVals()
+    readVals()
+    sendVals()
+    showVals()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,43 +1,58 @@
+function readVals () {
+    for (let index = 0; index <= 1; index++) {
+        // P2:0 左ジョイスティック選択
+        // P2:1 右ジョイスティック選択
+        pins.digitalWritePin(DigitalPin.P2, index)
+        vals[index] = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, -255, 255)
+    }
+}
+function saveVals () {
+    for (let index2 = 0; index2 <= 1; index2++) {
+        prevVals[index2] = vals[index2]
+    }
+}
+function sendVals () {
+    for (let index3 = 0; index3 <= 1; index3++) {
+        radio.sendValue(keys[index3], Math.round(vals[index3]))
+    }
+}
+function showVals () {
+    for (let index4 = 0; index4 <= 1; index4++) {
+        if (prevVals[index4] != vals[index4]) {
+            col = cols[index4]
+            led.unplot(col, Math.round(Math.map(prevVals[index4], -255, 255, 4, 0)))
+            led.plot(col, Math.round(Math.map(vals[index4], -255, 255, 4, 0)))
+        }
+    }
+}
+function setup () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    radio.setGroup(12)
+    vals = [0, 0]
+    prevVals = [0, 0]
+    cols = [0, 4]
+    keys = ["L", "R"]
+}
 /**
  * ジョイスティック：
  * 
  * https://www.switch-science.com/catalog/5308/
  */
-let PREV_R = 0
-let PREV_L = 0
-let DISP_R = 0
-let DISP_L = 0
-let R = 0
-let L = 0
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-radio.setGroup(12)
+let cols: number[] = []
+let col = 0
+let keys: string[] = []
+let prevVals: number[] = []
+let vals: number[] = []
+setup()
 basic.forever(function () {
-    // P2:0 左ジョイスティック選択
-    // P2:1 右ジョイスティック選択
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    // P0は横方向
-    // P1は縦方向
-    L = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, -100, 100)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    R = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, -100, 100)
-    radio.sendValue("L", L)
-    radio.sendValue("R", R)
-    DISP_L = Math.round(Math.map(L, -100, 100, 4, 0))
-    DISP_R = Math.round(Math.map(R, -100, 100, 4, 0))
-    if (DISP_L != PREV_L) {
-        led.unplot(0, PREV_L)
-        led.plot(0, DISP_L)
-    }
-    if (DISP_R != PREV_R) {
-        led.unplot(4, PREV_R)
-        led.plot(4, DISP_R)
-    }
-    PREV_L = DISP_L
-    PREV_R = DISP_R
+    saveVals()
+    readVals()
+    sendVals()
+    showVals()
 })
